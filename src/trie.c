599ccebee4befef977bcab9792fae2dd6e325bd8
deter0 <deter0109@gmail.com>
2024-01-21 08:12:27 -0500

    Finish Trie Implementation

--- src/trie.c
+++ src/trie.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "fruits.h"
 
@@ -8,7 +10,10 @@
 
 typedef struct Node Node;
 struct Node {
+  bool word;
+  char character;
   struct Node *children[256];
+  Node *parent;
 };
 
 #define NODE_POOL_CAP 1024
@@ -30,9 +35,110 @@ void insert_text(Node *root, const char *text) {
 
   if (root->children[index] == NULL) {
     root->children[index] = alloc_node();
+    root->children[index]->parent = root;
   }
 
+  root->children[index]->character = *text;
+  
+  if (*(text + 1) == 0) {
+    root->children[index]->word = true;
+  }
   insert_text(root->children[index], text + 1);
+}
+
+void strrev(char* str)
+{
+  // if the string is empty
+  if (!str) {
+    return;
+  }
+  // pointer to start and end at the string
+  int i = 0;
+  int j = strlen(str) - 1;
+ 
+  // reversing string
+  while (i < j) {
+    char c = str[i];
+    str[i] = str[j];
+    str[j] = c;
+    i++;
+    j--;
+  }
+}
+
+#define MAX_WORDS_PER_POOL 128
+typedef struct {
+  char *words[MAX_WORDS_PER_POOL];
+  size_t words_count;
+} WordPool;
+
+void delete_word_pool(WordPool *word_pool) {
+  for (size_t i = 0; i < word_pool->words_count; i++) {
+    free(word_pool->words[i]);
+  }
+}
+
+char *collect_word(Node *node) {
+  Node *working_node = node;
+
+  size_t word_alloc = 0;
+  
+  while (working_node->parent != NULL) {
+    word_alloc++;
+    working_node = working_node->parent;
+  }
+
+  if (word_alloc <= 0) { return NULL; }
+  
+  char *word_buffer = malloc(word_alloc + 1);
+  size_t word_size = 0;
+  
+  assert(word_buffer != NULL);
+
+  working_node = node;
+  while (working_node->parent != NULL) {
+    assert(word_size < word_alloc);
+    word_buffer[word_size++] = working_node->character;
+    working_node = working_node->parent;
+  }
+
+  strrev(word_buffer);
+
+  word_buffer[word_alloc] = 0;
+
+  return word_buffer;
+}
+
+void search_down_for_words(Node *root, WordPool *word_pool) {
+  if (root->word == true) {
+    char *word = collect_word(root);
+    word_pool->words[word_pool->words_count++] = word;
+    if (word_pool->words_count >= MAX_WORDS_PER_POOL) {
+      printf("Max search reached.\n");
+      return;
+    }
+  }
+
+  for (size_t i = 0; i < ARRAY_LEN(root->children); i++) {
+    if (root->children[i] != NULL) {
+      search_down_for_words(root->children[i], word_pool);
+    }
+  }
+}
+
+void search_trie(Node *root, const char *text, WordPool *search_results) {
+  Node *furthest_node = root;
+
+  for (size_t i = 0; i < strlen(text); i++) {
+    Node *child = furthest_node->children[(size_t)text[i]];
+    if (child == NULL) {
+      break;
+    } else {
+      furthest_node = child;
+    }
+  }
+  
+  search_down_for_words(furthest_node, search_results);
 }
 
 void dump_dot(Node *root) {
@@ -42,7 +148,7 @@ void dump_dot(Node *root) {
     if (root->children[i] != NULL) {
       size_t child_index = root->children[i] - node_pool;
       printf("    Node_%zu [label=\"%c\"]\n", child_index, (char)i);
-      printf("    Node_%zu -> Node_%zu [label=\"%c\"]\n", index, child_index, (char)i);
+      printf("    Node_%zu -> Node_%zu [label=\"%s\"]\n", index, child_index, root->children[i]->word ? "WORD" : (char[2]){i, 0});
       dump_dot(root->children[i]);
     }
   }
@@ -57,9 +163,29 @@ int main(void) {
     insert_text(root, fruits[i]);
   }
 
-  printf("digraph Trie {\n");
-  dump_dot(root);
-  printf("}\n");
+  assert(root->children['A']);
+  assert(!root->children['z']);
+  assert(root->children['A']->children['p']->children['p']->character == 'p');
+  assert(root->children['A']->children['p']->children['p']->word == false);
+  assert(root->children['A']->children['p']->children['p']->children['l']->children['e']->word == true);
+
+  //printf("digraph Trie {\n");
+  //dump_dot(root);
+  //printf("}\n");
+
+  char *word = collect_word(root->children['A']->children['p']->children['p']->children['l']->children['e']);
+  assert(strcmp(word, "Apple") == 0);
+  assert(strlen(word) == 5);
+  free(word);
+
+  WordPool search_results = { 0 };
+  search_trie(root, "C", &search_results);
+
+  for (size_t i = 0; i < search_results.words_count; i++) {
+    printf("Result: %s\n", search_results.words[i]);
+  }
+
+  delete_word_pool(&search_results);
   
   return 0;
 }
--- trie_build.sh
+++ trie_build.sh
@@ -6,6 +6,6 @@ set -xe
 CFLAGS="-Wall -Wextra -Werror -std=c11 -pedantic -ggdb"
 cc $CFLAGS ./src/trie.c -o trie
 
-./trie > trie.dot
-dot -Tsvg trie.dot > trie.svg
+ps ./trie # > trie.dot
+# dot -Tsvg trie.dot > trie.svg
 
